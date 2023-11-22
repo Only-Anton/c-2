4689d73d9075b7081f47af39b99667d818b80c13
Name <123@mail.ru>
2023-11-22 10:20:34 +0300

    Сравнили WriteLine & Write

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Hello, World!");
+//Console.Write("Hello, World!");
+Console.Write("Replay: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Начали абзац...");
+int n=0;
+while (n<a)
+{
+    Console.Write("пишем...");
+    n+=1;
+}
+Console.WriteLine("закончили абзац.");
+Console.Write("Начали абзац...");
+while (n>0)
+{
+    Console.Write("пишем...");
+    n-=1;
+}
+Console.WriteLine("закончили абзац.");
+Console.Write("Новый абзац...");
